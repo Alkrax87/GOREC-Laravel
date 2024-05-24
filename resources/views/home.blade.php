a86ffdc0af8e45f7fbaeb4ee860c06319c185507
Alkrax87 <thedarkdawn87gtamc@gmail.com>
2024-05-24 10:49:05 -0500

    Instalacion de adminlte y seteo en el home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,18 +1,24 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Home</title>
-</head>
-<body>
-  <div class="container">
-    <h2>Welcome to Home</h2>
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+    <h1>Dashboard</h1>
+@stop
+
+@section('content')
+    <p>Welcome to this beautiful admin panel.</p>
     <form action="{{ route('logout') }}" method="POST">
-        @csrf
-        <button type="submit">Logout</button>
+      @csrf
+      <button type="submit">Logout</button>
     </form>
-</div>
-</body>
-</html>
+@stop
+
+@section('css')
+    {{-- Add here extra stylesheets --}}
+    {{-- <link rel="stylesheet" href="/css/admin_custom.css"> --}}
+@stop
+
+@section('js')
+    <script> console.log("Hi, I'm using the Laravel-AdminLTE package!"); </script>
+@stop
