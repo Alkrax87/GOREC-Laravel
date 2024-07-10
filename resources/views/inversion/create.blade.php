cf93d69a837187b20b6f82a58e57e50ac7661f45
Alkrax87 <thedarkdawn87gtamc@gmail.com>
2024-07-09 19:26:32 -0500

    Fix interfaz

--- resources/views/inversion/create.blade.php
+++ resources/views/inversion/create.blade.php
@@ -163,4 +163,29 @@
       });
     });
   </script>
+  <style>
+    .input-auth {
+      display: block;
+      width: 100%;
+      height: calc(1.5em + 0.75rem + 2px);
+      padding: 0.375rem 0.75rem;
+      font-size: 1rem;
+      font-weight: 400;
+      line-height: 1.5;
+      color: #495057;
+      background-color: #fff;
+      background-clip: padding-box;
+      border: 1px solid #ced4da;
+      border-radius: 0.25rem;
+      transition: all 0.3s ease-in-out;
+    }
+    .input-auth:focus {
+      border-color: #72081f;
+      outline: none;
+      box-shadow: 0 0 5px 2px rgba(255, 106, 133, 0.5);
+    }
+    .input-autht:focus::placeholder {
+      color: transparent;
+    }
+  </style>
 </form>
