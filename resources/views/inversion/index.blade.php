bb63edbc048dd5886e407db8b53464c2ba48a64b
Alkrax87 <thedarkdawn87gtamc@gmail.com>
2024-12-09 21:28:51 -0500

    Correcion errores merge

--- resources/views/inversion/index.blade.php
+++ resources/views/inversion/index.blade.php
@@ -80,7 +80,7 @@
                   <td>{{ $inversion->nombreInversion }}</td>
                   <td>{{ $inversion->nombreCortoInversion }}</td>
                   <td class="text-nowrap text-center">{{ $inversion->usuario->nombreUsuario . ' ' . $inversion->usuario->apellidoUsuario }}</td>
-                  <td class="text-nowrap text-center">{{ $inversion->cordinador->nombreUsuario . ' ' . $inversion->cordinador->apellidoUsuario }}</td>
+                  <td class="text-nowrap text-center">{{ optional($inversion->cordinador)->nombreUsuario . ' ' . optional($inversion->cordinador)->apellidoUsuario ?? '' }}</td>
                   <td class="project_progress text-nowrap">
                     <div class="progress">
                       <div class="progress-bar progress-bar-striped
