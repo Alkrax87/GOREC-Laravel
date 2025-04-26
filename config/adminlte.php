9138b83588362a0add1a3793cf7b7dc72688bd8f
Alkrax87 <miguelavp_hl@outlook.com>
2025-04-26 12:59:19 -0500

    Cambios clases sidebar

--- config/adminlte.php
+++ config/adminlte.php
@@ -198,7 +198,7 @@ return [
     'classes_content_wrapper' => '',
     'classes_content_header' => '',
     'classes_content' => '',
-    'classes_sidebar' => 'sidebar-dark-primary elevation-4',
+    'classes_sidebar' => 'bg-aside',
     'classes_sidebar_nav' => '',
     'classes_topnav' => 'navbar-white navbar-light',
     'classes_topnav_nav' => 'navbar-expand',
